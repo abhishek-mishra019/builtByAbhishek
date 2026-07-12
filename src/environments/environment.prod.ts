9462a5b855c3f668e2d73d2ddbb37d1321dea82a
abhishek-mishra019 <am1426620@gmail.com>
2026-07-12 22:01:07 +0530

    security

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,7 @@
 export const environment = {
     production: true,
     firebase: {
-  apiKey: "AIzaSyCeESyIfjul1cxvoSpM-Fvyzs37cmnpScs",
-  authDomain: "portfolio-34c3b.firebaseapp.com",
-  projectId: "portfolio-34c3b",
-  storageBucket: "portfolio-34c3b.firebasestorage.app",
-  messagingSenderId: "510379183265",
-  appId: "1:510379183265:web:c78d00f0a8c2e2852c74e0",
-  measurementId: "G-HGWXK0767P"
+
     },
   };
   
